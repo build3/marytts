f018f105d67e8a78e9e3c01818b4924c9c098f98
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-08-03 14:02:19 +0200

    Get audio response from marytts

---
+++ backend/marytts_processor/marytts.py
@@ -0,0 +1,29 @@
+import os
+import requests
+from typing import Tuple
+from urllib.parse import urlencode
+
+from .repository import BaseMaryTTSRepository
+
+
+MARYTTS_URL = os.environ['MARYTTS_URL']
+
+MARYTTS_PROCESS_ENDPOINT = 'process'
+
+
+class MaryTTSRepository(BaseMaryTTSRepository):
+    def voice_output(self, text: str) -> Tuple[bytes, str]:
+        query_hash = {
+            "INPUT_TEXT": text,
+            "INPUT_TYPE": "TEXT",
+            "LOCALE": "en_US",
+            "VOICE": "cmu-bdl-hsmm",
+            "OUTPUT_TYPE": "AUDIO",
+            "AUDIO": "WAVE"
+        }
+
+        query = urlencode(query_hash)
+
+        response = requests.post(f"{MARYTTS_URL}{MARYTTS_PROCESS_ENDPOINT}?{query}")
+
+        return response.content, response.headers['Content-Type']
