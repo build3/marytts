8693294ecbd77a388127310510f980eec2a58f79
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-08-03 14:03:42 +0200

    Add route to obtain audio voice

--- backend/gateway/__init__.py
+++ backend/gateway/__init__.py
@@ -1,4 +1,17 @@
-from flask import Blueprint
+from flask import abort, Blueprint, Response, request
+
+from marytts_processor import process_voice_output
 
 
 gateway_bp = Blueprint('mtts', __name__)
+
+
+@gateway_bp.route('/audio-voice', methods=['POST'])
+def get_voice_output():
+    text = request.json.get('input_text')
+
+    if not text:
+        abort(400, 'input_text property is required')
+
+    content, mimetype = process_voice_output(text)
+    return Response(content, mimetype=mimetype)
