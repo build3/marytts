26b0eb5ccf6313680fe68e632bb9b2a91b80b966
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-09-01 07:49:26 +0200

    Refactor other routes to use the MaryTTS form

--- backend/gateway/__init__.py
+++ backend/gateway/__init__.py
@@ -1,4 +1,5 @@
 from flask import abort, Blueprint, Response, request
+from werkzeug.datastructures import MultiDict
 
 from marytts_processor import get_xml, process_phonemes, process_voice_output
 
@@ -10,27 +11,23 @@ gateway_bp = Blueprint('mtts', __name__)
 
 @gateway_bp.route('/audio-voice', methods=['POST'])
 def get_voice_output():
-    text = request.json.get('input_text')
-    locale = request.json.get('locale', 'en_US')
-    voice = request.json.get('voice', 'cmu-bdl-hsmm')
+    form = MaryTTSForm(MultiDict(request.json), meta={'csrf': False})
 
-    if not text:
-        abort(400, 'input_text property is required')
+    if not form.validate():
+        abort(400, form.errors)
 
-    content, mimetype, status = process_voice_output(text, locale, voice)
+    content, mimetype, status = process_voice_output(form.data['input_text'], form.data['locale'], form.data['voice'])
     return Response(content, mimetype=mimetype, status=status)
 
 
 @gateway_bp.route('/phonemes', methods=['POST'])
 def get_phonemes():
-    text = request.json.get('input_text')
-    locale = request.json.get('locale', 'en_US')
-    voice = request.json.get('voice', 'cmu-bdl-hsmm')
+    form = MaryTTSForm(MultiDict(request.json), meta={'csrf': False})
 
-    if not text:
-        abort(400, 'input_text property is required')
+    if not form.validate():
+        abort(400, form.errors)
 
-    data, status = process_phonemes(text, locale, voice)
+    data, status = process_phonemes(form.data['input_text'], form.data['locale'], form.data['voice'])
 
     return Response(data, mimetype='application/json', status=status)
 
