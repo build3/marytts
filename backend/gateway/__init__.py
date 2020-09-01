8545c9d0bd16711b3927045f4b09ad190200a470
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-09-01 12:50:31 +0200

    Add route to obtain audio from XML

--- backend/gateway/__init__.py
+++ backend/gateway/__init__.py
@@ -1,6 +1,6 @@
 from flask import abort, Blueprint, Response, request
 
-from marytts_processor import process_phonemes, process_voice_output
+from marytts_processor import process_phonemes, process_voice_output, process_voice_output_from_xml
 
 
 gateway_bp = Blueprint('mtts', __name__)
@@ -31,3 +31,16 @@ def get_phonemes():
     data, status = process_phonemes(text, locale, voice)
 
     return Response(data, mimetype='application/json', status=status)
+
+
+@gateway_bp.route('/xml/audio-voice', methods=['POST'])
+def get_voice_output_from_xml():
+    xml_file = request.files.get('xml')
+
+    if not xml_file:
+        abort(400, {"xml": ["XML file is required."]})
+
+    xml = xml_file.read().decode('utf-8')
+
+    content, mimetype, status = process_voice_output_from_xml(xml)
+    return Response(content, mimetype=mimetype, status=status)
