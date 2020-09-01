8f5dcba4bb1ad20e91397e9c4b41d447a8f0c99b
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-09-01 12:45:05 +0200

    Export use case which generates voice from xml

--- backend/marytts_processor/__init__.py
+++ backend/marytts_processor/__init__.py
@@ -1,7 +1,8 @@
-from .use_cases import process_phonemes, process_voice_output
+from .use_cases import process_phonemes, process_voice_output, process_voice_output_from_xml
 
 
 __all__ = [
     'process_phonemes',
     'process_voice_output',
+    'process_voice_output_from_xml',
 ]
