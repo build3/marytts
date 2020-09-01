370cea8aa9f0ea47d9c564116d7bb989c57f4e4f
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-09-01 13:52:14 +0200

    Export use case which reads XML file

--- backend/marytts_processor/__init__.py
+++ backend/marytts_processor/__init__.py
@@ -1,8 +1,9 @@
-from .use_cases import process_phonemes, process_voice_output, process_voice_output_from_xml
+from .use_cases import *
 
 
 __all__ = [
     'process_phonemes',
     'process_voice_output',
     'process_voice_output_from_xml',
+    'process_phonemes_from_xml',
 ]
