71f42ecf91cd43852c59b01d2d639e49931f3d4a
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-09-01 07:09:04 +0200

    Draft route for obtaining XML file

--- backend/gateway/__init__.py
+++ backend/gateway/__init__.py
@@ -31,3 +31,15 @@ def get_phonemes():
     data, status = process_phonemes(text, locale, voice)
 
     return Response(data, mimetype='application/json', status=status)
+
+
+@gateway_bp.route('/phonemes', methods=['POST'])
+def get_xml_from_marytts():
+    text = request.json.get('input_text')
+    locale = request.json.get('locale', 'en_US')
+    voice = request.json.get('voice', 'cmu-bdl-hsmm')
+
+    if not text:
+        abort(400, 'input_text property is required')
+
+    return Response(data, mimetype='application/xml', status=status)
