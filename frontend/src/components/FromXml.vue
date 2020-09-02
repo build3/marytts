16db6e280e5211b618113ce22d896cf69a18e8d6
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-09-02 08:44:10 +0200

    Add basic file field to store XML file

--- frontend/src/components/FromXml.vue
+++ frontend/src/components/FromXml.vue
@@ -1,6 +1,18 @@
 <template>
     <div>
-        XML
+        <div class="file is-centered">
+            <label class="file-label">
+                <input class="file-input" type="file" name="resume">
+                <span class="file-cta">
+                    <span class="file-icon">
+                        <font-awesome-icon icon="upload" />
+                    </span>
+                    <span class="file-label">
+                        Choose a XML file…
+                    </span>
+                </span>
+            </label>
+        </div>
     </div>
 </template>
 
