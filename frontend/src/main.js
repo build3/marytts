c341768464fb4b259d39b1665a7d5c50a9a941d6
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-09-02 08:43:49 +0200

    Add font awesome to the project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from "vue";
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUpload } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import App from './App.vue';
 import store from './store';
 import Tabs from './components/Tabs.vue';
@@ -6,10 +11,14 @@ import FromXml from './components/FromXml.vue';
 
 import './assets/styles/main.scss';
 
+library.add(faUpload);
+
 const app = createApp(App);
 
 app.component('mary-tabs', Tabs);
 app.component('from-xml', FromXml);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.use(store);
 app.mount("#app");
