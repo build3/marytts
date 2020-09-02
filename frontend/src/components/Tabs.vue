231ecaf47382177025a6c0303932f0532a7a0cb2
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-09-02 08:04:25 +0200

    Draft tab component

---
+++ frontend/src/components/Tabs.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="tabs is-boxed is-centered is-fullwidth">
+        <ul>
+            <li class="is-active"><a><span>Text</span></a></li>
+            <li><a><span>XML</span></a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { textTab, xmlTab } from '../store/index';
+
+export default {
+    setup() {
+        
+    }
+}
+</script>
