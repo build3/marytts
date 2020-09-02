c4e8e03b017bf60cb48a25635becacf1b7ae2e1f
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-09-02 08:05:42 +0200

    Set is-active class if tab is active

--- frontend/src/components/Tabs.vue
+++ frontend/src/components/Tabs.vue
@@ -1,18 +1,41 @@
 <template>
     <div class="tabs is-boxed is-centered is-fullwidth">
         <ul>
-            <li class="is-active"><a><span>Text</span></a></li>
-            <li><a><span>XML</span></a></li>
+            <li
+                :class="{ 'is-active': activeTab === textTab }"
+            >
+                <a>
+                    <span>Text</span>
+                </a>
+            </li>
+            <li
+                :class="{ 'is-active': activeTab === xmlTab }"
+            >
+                <a>
+                    <span>XML</span>
+                </a>
+            </li>
         </ul>
     </div>
 </template>
 
 <script>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
 import { textTab, xmlTab } from '../store/index';
 
 export default {
     setup() {
-        
+        const store = useStore();
+
+        const activeTab = computed(() => store.getters.activeTab);
+
+        return {
+            activeTab,
+            textTab,
+            xmlTab,
+        }
     }
 }
 </script>
