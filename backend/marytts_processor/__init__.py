1d25265a04beb75a21a15a904b773297f1cb5e27
Rafał Kowalski <rafal.kowalski@makimo.pl>
2021-06-10 16:15:32 +0200

    Add use case to process simplified audio based on text

--- backend/marytts_processor/__init__.py
+++ backend/marytts_processor/__init__.py
@@ -1,10 +1,13 @@
 from .use_cases import *
-
+from .generator import Point
 
 __all__ = [
     'get_xml',
     'process_phonemes',
+    'process_simplified_phonemes',
     'process_voice_output',
+    'process_simplified_voice_output',
     'process_voice_output_from_xml',
     'process_phonemes_from_xml',
+    'Point',
 ]
--- backend/marytts_processor/use_cases.py
+++ backend/marytts_processor/use_cases.py
@@ -1,6 +1,8 @@
 import re
+import json
 from typing import Any, Tuple
 
+from .generator import MaryTTSXMLGenerator, Point
 from .marytts import MaryTTSRepository
 from .processor import MaryTTSXMLProcessor
 from .repository import BaseMaryTTSRepository
@@ -13,6 +15,47 @@ def process_voice_output(
     repository: BaseMaryTTSRepository = MaryTTSRepository()
 ) -> Tuple[bytes, str, int]:
     return repository.voice_output(text, locale, voice)
+
+
+def process_simplified_voice_output(
+    text: str,
+    locale: str,
+    voice: str,
+    repository: BaseMaryTTSRepository = MaryTTSRepository()
+) -> Tuple[bytes, str, int]:
+    original_content, original_status_code, _ = repository.load_acoustic_params(text, locale, voice)
+
+    if original_status_code != 200:
+        raise Exception("MaryTTS return incorrect acoustic params")
+
+    points = json.loads(MaryTTSXMLProcessor(original_content).process(should_simplify=True))
+
+    modifiers = [
+        Point(
+            hz=point['hertz'],
+            ms=point['ms'],
+            phoneme=point['phoneme_name']
+        ) for point in points
+    ]
+
+    xml = MaryTTSXMLGenerator(original_content, modifiers).generate()
+    return repository.voice_output_from_xml(xml, locale, voice)
+
+
+def process_voice_output_from_text_and_points(
+    text: str,
+    modifiers: Point,
+    locale: str,
+    voice: str,
+    repository: BaseMaryTTSRepository = MaryTTSRepository()
+) -> Tuple[bytes, str, int]:
+    original_content, original_status_code, _ = repository.load_acoustic_params(text, locale, voice)
+
+    if original_status_code != 200:
+        raise Exception("MaryTTS return incorrect acoustic params")
+
+    xml = MaryTTSXMLGenerator(original_content, modifiers).generate()
+    return repository.voice_output_from_xml(xml, locale, voice)
 
 
 def process_phonemes(
@@ -28,7 +71,23 @@ def process_phonemes(
 
     processor = MaryTTSXMLProcessor(content)
 
-    return processor.process(), status_code
+    return processor.process(should_simplify=False), status_code
+
+
+def process_simplified_phonemes(
+    text: str,
+    locale: str,
+    voice: str,
+    repository: BaseMaryTTSRepository = MaryTTSRepository()
+)-> Tuple[str, int]:
+    content, status_code, _ = repository.load_acoustic_params(text, locale, voice)
+
+    if not status_code == 200:
+        return content.decode('utf-8'), status_code
+
+    processor = MaryTTSXMLProcessor(content)
+
+    return processor.process(should_simplify=True), status_code
 
 
 def get_xml(
@@ -50,4 +109,4 @@ def process_voice_output_from_xml(
 
 
 def process_phonemes_from_xml(xml: bytes) -> Tuple[bytes, str, int]:
-    return MaryTTSXMLProcessor(xml).process(), 200
+    return MaryTTSXMLProcessor(xml).process(should_simplify=False), 200
